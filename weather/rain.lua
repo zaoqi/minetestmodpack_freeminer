89de7a4f0a28ba221a1f7673b082ad994f311752
zaoqi <zaoqiff@gmail.com>
2017-01-21 18:10:02 +0800

    增加technic

--- weather/rain.lua
+++ weather/rain.lua
@@ -1,24 +1,104 @@
 -- Rain
-minetest.register_globalstep(function(dtime)
-	if weather ~= "rain" then return end
-	for _, player in ipairs(minetest.get_connected_players()) do
+core.register_globalstep(function(dtime)
+	for _, player in ipairs(core.get_connected_players()) do
 		local ppos = player:getpos()
-
+		local strength = get_rain(ppos)
+		if strength > 0 and core.get_node(ppos).name == "air" then
+--print("rain he=".. core.get_heat(ppos).." hu=".. core.get_humidity(ppos) .. " s=" .. strength)
 		-- Make sure player is not in a cave/house...
-		if minetest.env:get_node_light(ppos, 0.5) ~= 15 then return end
+		if core.get_node_light(ppos, 0.5) ~= default.LIGHT_SUN then return end
 
 		local minp = addvectors(ppos, {x=-9, y=7, z=-9})
 		local maxp = addvectors(ppos, {x= 9, y=7, z= 9})
 
+--[[ old
 		local vel = {x=0, y=   -4, z=0}
 		local acc = {x=0, y=-9.81, z=0}
 
-		minetest.add_particlespawner({amount=25, time=0.5,
+		core.add_particlespawner({
+			amount=10*strength, time=0.5,
 			minpos=minp, maxpos=maxp,
 			minvel=vel, maxvel=vel,
 			minacc=acc, maxacc=acc,
 			minexptime=0.8, maxexptime=0.8,
 			minsize=25, maxsize=25,
-			collisiondetection=false, vertical=true, texture="weather_rain.png", player=player:get_player_name()})
+			collisiondetection=false, 
+			vertical=true,
+			texture="weather_rain.png",
+			player=player:get_player_name()
+		})
+]]
+
+	       local minpos = addvectors(player:getpos(), {x = -15, y = 15, z = -15})
+	       local maxpos = addvectors(player:getpos(), {x = 15, y = 10, z = 15})
+	       core.add_particlespawner(
+		  {
+		     amount = 30*strength,
+		     time = 0.5,
+		     minpos = minpos,
+		     maxpos = maxpos,
+		     minvel = {x = 0, y = -20, z = 0},
+		     maxvel = {x = 0, y = -20, z = 0},
+		     minexptime = 0.9,
+		     maxexptime = 1.1,
+		     minsize = 2,
+		     maxsize = 3,
+		     collisiondetection = false, --true,
+		     vertical = true,
+		     texture = "weather_rain.png",
+		     playername = player:get_player_name()
+		  }
+		  )
+
+
+
+		end
 	end
 end)
+
+core.register_abm({
+	nodenames = {"group:crumbly", "group:snappy", "group:cracky", "group:choppy", "group:water"},
+	neighbors = {"air"},
+	interval = 15.0,
+	chance = 50,
+	action = function (pos, node, active_object_count, active_object_count_wider)
+		-- todo! chance must depend on rain value
+		local amount = get_rain(pos)
+		if amount == 0 then return end
+		amount = amount * 3
+		if amount < 1 then amount = 1 end
+		if core.registered_nodes[node.name].drawtype ~= "normal"
+			and core.registered_nodes[node.name].drawtype ~= "nodebox"
+			and core.registered_nodes[node.name].drawtype ~= "flowingliquid"
+			and core.registered_nodes[node.name].drawtype ~= "liquid"
+			and core.registered_nodes[node.name].drawtype ~= "allfaces_optional" then  return end
+		local np = addvectors(pos, {x=0, y=1, z=0})
+		if core.get_node_light(np, 0.5) ~= default.LIGHT_SUN then return end
+			if core.get_node(pos).name == "default:water_flowing" then
+				core.add_node_level(pos, 4*amount)
+			elseif core.get_node(np).name == "air" then
+				core.set_node(np, {name="water_flowing"})
+				core.set_node_level(np, amount)
+			end
+	end
+})
+
+-- evaporate
+core.register_abm({
+	nodenames = {"default:water_flowing"},
+	neighbors = {"air"},
+	interval = 10.0,
+	chance = 10,
+	action = function (pos, node, active_object_count, active_object_count_wider)
+		-- todo! chance must depend on humidity and temperature
+		local humidity = core.get_humidity(pos)
+		if get_rain(pos) > 0 or humidity > 90 then return end
+		local np = addvectors(pos, {x=0, y=1, z=0})
+		--if core.get_node_light(np, 0.5) == default.LIGHT_SUN then
+		if core.get_node(np).name == "air" then
+		local amount = ((100-humidity)/20)
+		if amount < 1 then amount = 1 end
+			core.add_node_level(pos, -amount)
+		end
+	end
+})
--- weather/snow.lua
+++ weather/snow.lua
@@ -1,11 +1,12 @@
 -- Snow
-minetest.register_globalstep(function(dtime)
-	if weather ~= "snow" then return end
-	for _, player in ipairs(minetest.get_connected_players()) do
+core.register_globalstep(function(dtime)
+	for _, player in ipairs(core.get_connected_players()) do
 		local ppos = player:getpos()
-
+		local strength = get_snow(ppos)
+		if strength > 0 and core.get_node(ppos).name == "air" then
+--print("snow he=".. core.get_heat(ppos).." hu=".. core.get_humidity(ppos) .. " s=" .. strength)
 		-- Make sure player is not in a cave/house...
-		if minetest.env:get_node_light(ppos, 0.5) ~= 15 then return end
+		if core.get_node_light(ppos, 0.5) ~= default.LIGHT_SUN then return end
 
 		local minp = addvectors(ppos, {x=-9, y=7, z=-9})
 		local maxp = addvectors(ppos, {x= 9, y=7, z= 9})
@@ -16,21 +17,58 @@ minetest.register_globalstep(function(dtime)
 		local vel = {x=0, y=   -0.5, z=0}
 		local acc = {x=0, y=   -0.5, z=0}
 
-		minetest.add_particlespawner(5, 0.5,
-			minp, maxp,
-			vel, vel,
-			acc, acc,
-			5, 5,
-			25, 25,
-			false, "weather_snow.png", player:get_player_name())
+--[[
+		core.add_particlespawner({
+			amount=5*strength, time=0.5,
+			minpos=minp, maxpos=maxp,
+			minvel=vel, maxvel=vel,
+			minacc=acc, maxacc=acc,
+			minexptime=5, maxexptime=5,
+			minsize=25, maxsize=25,
+			collisiondetection=false,
+			texture="weather_snow.png",
+			player=player:get_player_name()
+		})
 
-		minetest.add_particlespawner(4, 0.5,
-			minp_deep, maxp_deep,
-			vel, vel,
-			acc, acc,
-			4, 4,
-			25, 25,
-			false, "weather_snow.png", player:get_player_name())
+		core.add_particlespawner({
+			amount=4*strength, time=0.5,
+			minpos=minp, maxpos=maxp,
+			minvel=vel, maxvel=vel,
+			minacc=acc, maxacc=acc,
+			minexptime=4, maxexptime=4,
+			minsize=25, maxsize=25,
+			collisiondetection=false,
+			texture="weather_snow.png",
+			player=player:get_player_name()
+		})
+]]
+	       local minpos = addvectors(player:getpos(), {x = -30, y = 20, z = -30})
+	       local maxpos = addvectors(player:getpos(), {x = 30, y = 15, z = 30})
+	       local vel = {x = 16.0, y = -8, z = 13.0}
+	       local acc = {x = -16.0, y = -8, z = -13.0}
+	       core.add_particlespawner(
+		  {
+		     amount = 8*strength,
+		     time = 0.4,
+		     minpos = minpos,
+		     maxpos = maxpos,
+		     minvel = {x=-vel.x, y=vel.y, z=-vel.z},
+		     maxvel = vel,
+		     minacc = acc,
+		     maxacc = acc,
+		     minexptime = 1.0,
+		     maxexptime = 1.4,
+		     minsize = 3,
+		     maxsize = 4,
+		     collisiondetection = true,
+		     vertical = false,
+		     texture = "weather_snow.png",
+		     playername = player:get_player_name()
+		  }
+	       )
+
+
+		end
 	end
 end)
 
@@ -40,34 +78,58 @@ local snow_box =
 	fixed = {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}
 }
 
--- Snow cover
-minetest.register_node("weather:snow_cover", {
-	tiles = {"weather_snow_cover.png"},
-	drawtype = "nodebox",
-	paramtype = "light",
-	node_box = snow_box,
-	selection_box = snow_box,
-	groups = {not_in_creative_inventory = 1, crumbly = 3, attached_node = 1},
-	drop = {}
-})
 
---[[ Enable this section if you have a very fast PC
-minetest.register_abm({
-	nodenames = {"group:crumbly", "group:snappy", "group:cracky", "group:choppy"},
-	neighbors = {"default:air"},
-	interval = 10.0, 
-	chance = 80,
+default.time_speed = tonumber(core.setting_get("time_speed"))
+
+-- -[[ Enable this section if you have a very fast PC
+core.register_abm({
+	nodenames = {"group:crumbly", "group:snappy", "group:cracky", "group:choppy", "group:melts"},
+	neighbors = {"air"},
+	interval = 10.0,
+	chance = 50,
+
 	action = function (pos, node, active_object_count, active_object_count_wider)
-		if weather == "snow" then
-			if minetest.registered_nodes[node.name].drawtype == "normal"
-			or minetest.registered_nodes[node.name].drawtype == "allfaces_optional" then
-				local np = addvectors(pos, {x=0, y=1, z=0})
-				if minetest.env:get_node_light(np, 0.5) == 15
-				and minetest.env:get_node(np).name == "air" then
-					minetest.env:add_node(np, {name="weather:snow_cover"})
+		local amount = get_snow(pos)
+		if amount == 0 then return end
+		local add = 1 + (amount * 2);
+		if core.registered_nodes[node.name].drawtype ~= "normal"
+			and core.registered_nodes[node.name].drawtype ~= "nodebox"
+			and core.registered_nodes[node.name].drawtype ~= "allfaces_optional" then return end
+		local np = addvectors(pos, {x=0, y=1, z=0})
+		if core.get_node_light(np, 0.5) ~= default.LIGHT_SUN then return end
+		if core.get_node(pos).name == "default:snow" then
+			local min_level = core.get_node_level(pos)
+			local min_pos = pos
+
+			-- smooth
+			--local rnd = math.random(1, 4)
+			for rnd = 1, 4 do
+				if min_level <= 1 then break end
+				local addv = {x=0, y=0, z=0}
+				if     rnd == 1 then addv.x = 1
+				elseif rnd == 2 then addv.x = -1
+				elseif rnd == 3 then addv.z = -1
+				elseif rnd == 4 then addv.z = 1 end
+				local ngp = addvectors(min_pos, addv)
+				local lev = core.get_node_level(ngp)
+				if core.get_node(ngp).name == "default:snow" and lev < min_level then
+					min_level = lev
+					min_pos = ngp
 				end
 			end
+
+			pos = min_pos
+			np = addvectors(pos, {x=0, y=1, z=0})
+			add = core.add_node_level(pos, add);
+			if default.time_speed <= 0 then add = 0 end
+			if add > 0 then
+				core.set_node(pos, {name="default:ice"})
+			end
+		end
+		if add > 0 and core.get_node(np).name == "air" then
+			core.set_node(np, {name="snow"})
+			core.add_node_level(np, add)
 		end
 	end
 })
-]]
+--]]
